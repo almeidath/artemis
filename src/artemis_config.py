e954dd705bdb824d9baf918768bf6fe126dad791
Thiago Almeida <69994151+almeidath@users.noreply.github.com>
2021-09-09 21:42:31 -0300

    Update artemis_config.py

--- src/artemis_config.py
+++ src/artemis_config.py
@@ -1,6 +1,5 @@
 #Configure this file before you start trading
 
-
 #Write your Binance API key and secret here. 
 #This script will only use it to manage the trades. You should never share those informations with anyone
 API_KEY=""
